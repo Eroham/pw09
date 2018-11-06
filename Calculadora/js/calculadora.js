94d98b165b0759f17a6b5d8c737a6ae92c8f66bb
CCDM15 <ccdm15@CCDM15.local>
2018-11-06 10:00:07 -0700

    Calculadora Sin terminar

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -1,4 +1,5 @@
 window.onload=function(){
+
 	var colorAmarillo = function (){
 	this.style.background = "yellow";
 }
@@ -14,39 +15,101 @@ var	operador = ""
 var numeros = function () {
 	var valor= this.value;
 	if (operador=="") { //operando1
-		var valorInput=document.getElement("operando1").value;
+		var valorInput=document.getElementById("operando1").value;
 		if (valorInput=="0") {
 			document.getElementById("operando1").value="";
 		}
 
 		document.getElementById("operando1").valuet=valor;
 	}else { // operando1
-		if (operador=="") {
-		var valorInput=document.getElement("operando2").value;
+		var valorInput=document.getElementById("operando2").value;
 		if (valorInput=="0") {
 			document.getElementById("operando2").value="";
 		}
 		document.getElementById("operando2").valuet=valor;
 	}
+}
+	var operadores = function(){
+		operador = this.value
+	}
+
+	var resultadoigual = function(){
+		oper1=document.getElementById('operando1').value;
+		oper2=document.getElementById('operando2').value;
+		document.getElementById('resultado').value=eval(oper1+operador+oper2)
+
+	}
+
+	var reinicio = function(){
+		document.getElementById('operando1').value="0"
+		document.getElementById('operando2').value="0"
+		document.getElementById('resultado').value="0"
+		operador=""
+
+	}
 
 	var uno=document.getElementById("uno")
 	uno.addEventListener("click",numeros)
 	var dos=document.getElementById("dos")
-	uno.addEventListener("click",numeros)
+	dos.addEventListener("click",numeros)
 	var tres=document.getElementById("tres")
-	uno.addEventListener("click",numeros)
+	tres.addEventListener("click",numeros)
 	var cuatro=document.getElementById("cuatro")
-	uno.addEventListener("click",numeros)
+	cuatro.addEventListener("click",numeros)
 	var cinco=document.getElementById("cinco")
-	uno.addEventListener("click",numeros)
+	cinco.addEventListener("click",numeros)
 	var seis=document.getElementById("seis")
-	uno.addEventListener("click",numeros)
+	seis.addEventListener("click",numeros)
 	var siete=document.getElementById("siete")
-	uno.addEventListener("click",numeros)
+	siete.addEventListener("click",numeros)
 	var ocho=document.getElementById("ocho")
-	uno.addEventListener("click",numeros)
+	ocho.addEventListener("click",numeros)
 	var nueve=document.getElementById("nueve")
-	uno.addEventListener("click",numeros)
+	nueve.addEventListener("click",numeros)
+	var cero=document.getElementById("cero")
+	cero.addEventListener("click",numeros)
+	var suma=document.getElementById("suma")
+	suma.addEventListener("click",suma)
+	var resta=document.getElementById("resta")
+	resta.addEventListener("click",resta)
+	var multiplicacion=document.getElementById("multiplicacion")
+	multiplicacion.addEventListener("click",multiplicacion)
+	var division=document.getElementById("division")
+	division.addEventListener("click",division)
+	var igual=document.getElementById("igual")
+	igual.addEventListener("click",igual)
+
+
+	operando1.addEventListener('focus' colorAmarillo);
+	operando1.addEventListener('focusout' colorBlanco);
+	operando2.addEventListener('focus' colorAmarillo);
+	operando2.addEventListener('focusout' colorBlanco);
+	resultado.addEventListener('focus' colorAmarillo);
+	resultado.addEventListener('focusout' colorBlanco);
+
+	cero.addEventListener('click',numeros);
+	uno.addEventListener('click',numeros);
+	dos.addEventListener('click',numeros);
+	tres.addEventListener('click',numeros);
+	cuatro.addEventListener('click',numeros);
+	cinco.addEventListener('click',numeros);
+	seis.addEventListener('click',numeros);
+	siete.addEventListener('click',numeros);
+	ocho.addEventListener('click',numeros);
+	nueve.addEventListener('click',numeros);
+
+
+	suma.addEventListener('click',operadores);
+	resta.addEventListener('click',operadores);
+	multiplicacion.addEventListener('click',operadores);
+	division.addEventListener('click',operadores);
+
+	igual.addEventListener('click',resultadoigual);
+	reiniciar.addEventListener('click',reinicio);
+
+
+
+
 	
 
 }
