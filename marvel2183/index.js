6dfc1cdfc79aa654e5ddcd27e0d9c8cd39bee833
CCDM15 <ccdm15@CCDM15.local>
2018-11-22 10:09:10 -0700

    Marvel

--- marvel2183/index.js
+++ marvel2183/index.js
@@ -1,36 +1,69 @@
+const {BrowserWindow}=require('electron').remote
+const app=require('electron').app
+const path=require('path')
+const url=require('url')
 
 var personaje=""
+let PantallaDetalle;
+
+
+var btnComics=document.getElementByClassName('btnComics')
+
+var buscaComics = function(){
+	//alert(this.value)
+	localStorage.setItem("indice",this.value);
+	localStorage.setItem("personaje",personaje)
+	PantallaDetalle=new BrowserWindow({width:400,height:425});
+	PantallaDetalle.loadURL(url.format({
+		pathname: path.join(__dirname,'PantallaDetalle.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+	PantallaDetalle.show();
+
+}
 
 var buscaPersonaje = function(){
 	personaje=document.getElementById('txtPersonaje').value;
-	var url='https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=thor'
+	var url='https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=';
 	fetch(url+personaje)
 	.then(datos=>datos.json())
 	.then(datos=>{
 		var cantidad=datos.data.count
 		var foto=''
-		document.getElementById('abajo').innerHTML = ''
+		document.getElementById('abajo').innerHTML= ''
 		for(let i=0;i<cantidad;i++){
-
-
-		foto=datos.data.results[0].thumbnail.path+"."+
-			 datos.data.results[0].extension
+			foto=datos.data.results[i].thumbnail.path+"."+
+		     datos.data.results[i].thumbnail.extension
 		 document.getElementById('abajo').innerHTML += `
-		 <article class="abajoIzquierda">
-					<img src="${foto}" class="imgFoto">
-				</article>
-				<article class="abajoDerecha">
-					<div class="txtNombre">${datos.data.results[0].name}</div>
-					<button class="btnComics" value="">Comics</button> 
-				</article>
-				<hr>
-				<br> 
-				`
-
+		 	<article class="abajoIzquierda">
+				<img src="${foto}" class="imgFoto">
+			</article>
+			<article class="abajoDerecha">
+				<div class="txtNombre">${datos.data.results[i].name}</div>
+				<button class="btnComics" value="${i}">Comics</button> 
+			</article>
+			<hr>
+			<br>
+		 `
+		}//Termina For
+		for(let i=0;i<btnComics.length;i++){
+			btnComics[i].addEventListener('click', buscaComics);
 		}
-
+		
 	})
 }
 
 var btnBuscar=document.getElementById('btnBuscar')
 btnBuscar.addEventListener('click',buscaPersonaje)
+
+
+
+
+
+
+
+
+
+
+
--- marvel2183/main.js
+++ marvel2183/main.js
@@ -1,60 +1,54 @@
-// cargar la aplicacion de electron
+//cargar la aplicación de electron
 const app=require('electron').app;
-// Crear ventanas del sistema operativo
+//Crear ventanas del sistema operativo
 const BrowserWindow=require('electron').BrowserWindow;
 //Ruta del sistema de archivos del S.O.
 const path=require('path');
 const url= require('url');
-//Constantes para imprimir en PDF
-const electron=require('electron');
-//Ssitema de archivos
-const fs=require('fs')
-//Sistema operativo
-const os=require('os')
-//Para declarar una funcion remota
-//IPC = llamada a procedimiento interno
-const ipc=electron.ipcMain;s
-//Acceso a la terminal o linea de comandos
-const shell=electron.shell;
 
-
-
-
-
+//Constantes para PDF
+const electron = require('electron');
+//Sistema de archivos
+const fs = require('fs');
+//Acceso al sistema operativo
+const os = require('os');
+//Para declarar una función remota
+const ipc = electron.ipcMain;
+//Acceso a la terminal-línea de comandos
+const shell = electron.shell;
 
 //Otra forma de declarar una constante
 //Pantalla principal
 let PantallaPrincipal;
 
-
 function muestraPantallaPrincipal(){
-	//Creamos na pantalla vacia
-	PantallaPrincipal=new BrowserWindow({width:1042,height:450});
-	//Cargamos en la pantalla el contenido de nuestra pagina
+	//Creamos una pantalla vacía
+	PantallaPrincipal=new BrowserWindow({width:1024,height:850});
+	//Cargamos en la pantalla el contenido de nuestra página
 	PantallaPrincipal.loadURL(url.format({
 		pathname: path.join(__dirname,'index.html'),
 		protocol: 'file',
 		slashes: true
-
-	}))
+	}));
 	//Mostramos la pantalla
 	PantallaPrincipal.show();
-
 }
 
-//Eventos para PDF (declaracion)
+//Evento para PDF (declaración) 
 ipc.on('print-to-pdf',function(event){
 	const pdfPath=path.join(os.tmpdir(),'print.pdf')
 	const win=BrowserWindow.fromWebContents(event.sender)
 	win.webContents.printToPDF({},function(error,data){
 		if(error) throw error
 		fs.writeFile(pdfPath,data,function(error){
-			if (error) {
+			if(error){
 				throw error
 			}
 			shell.openExternal('file://'+pdfPath)
+			// win.close()
 		})
 	})
+});
 
-})
-app.on('ready', muestraPantallaPrincipal);
+app.on('ready',muestraPantallaPrincipal);
+
